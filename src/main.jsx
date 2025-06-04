bc2fe7f59bf57f8ad163995647371741669a4017
Chaitanya Bansal <bansalc586@gmail.com>
2025-06-04 13:44:23 +0530

    Save work before deploying

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import UserContext from './context/UserContext.jsx'
+import { Provider } from 'react-redux'
+import store from './Components/redux/store.js'
+import { HashRouter } from 'react-router-dom' 
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <UserContext>
+        <HashRouter>        
+          <App />
+        </HashRouter>
+      </UserContext>
+    </Provider>
+  </StrictMode>
+)
+
